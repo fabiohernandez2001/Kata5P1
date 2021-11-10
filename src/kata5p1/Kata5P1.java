e6ef2ea5a1647a126063aa5dc74776c42151ca21
Entrar <Entrar@sopa8>
2021-11-10 10:09:02 +0000

    Kata5P1 version 2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,11 +1,12 @@
 
 package kata5p1;
 
+import static kata5p1.Crear_Tabla.createNewTable;
+
 
 public class Kata5P1 {
     public static void main(String[] args) {
-        SelectApp app = new SelectApp();
-        app.selectAll();
+        createNewTable();
     }
     
 }
